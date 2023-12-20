e7d5358595c00d516dd1e8c53781a39f8cc6fb6c
Ушаков Вадим Сергеевич <v.ushakov@prosoftsystems.ru>
2023-12-20 08:52:27 +0500

    Borrowed soultion with slight improvement

--- problems/longest_pal_substring.hpp
+++ problems/longest_pal_substring.hpp
@@ -9,106 +9,36 @@ struct StrIndexes
     int end;
 };
 
-std::string getSubstring(const std::string& s, int start, int end)
+StrIndexes getPalindromicIdxs(const std::string& s, int left, int right)
 {
-    if (s.empty())
-        return "";
-    std::cout << "Str size: " << s.size() << " " << start << " " << end << "\n";
-    return std::string(s.c_str() + std::max(start, 0),
-        s.c_str() + std::min(end + 1, static_cast<int>(s.size())));
-}
-
-StrIndexes getRepeatingSequenceIndexes_(const std::string& s, int i)
-{
-    char c = s[i];
-    int s_size = s.size();
-    if (s[i - 1] != c && s[i + 1] != c)
-    {
-        return { i,i };
-    }
-    int left = i - 1, right = i + 1;
-    while (left - 1 > -1 && s[left - 1] == c)
-        left--;
-    while (right + 1 < s_size && s[right + 1] == c)
-        right++;
-    return { left, right };
-}
-
-StrIndexes getIndexesForEdges(const std::string& s, int i)
-{
-    int left = i - 1, right = i + 1;
-    if (s[left] == s[right])
-        return { left, right };
-    if (s[left] == s[i])
-        return { left, i };
-    if (s[right] == s[i])
-        return { i, right };
-    return { i, i };
-}
-
-StrIndexes getIndexesForCurrent_(const std::string& s, int i)
-{
-    int s_size = s.size();
-    if ((i - 1) == 0 || (i + 1) == s_size - 1)
-    {
-        return getIndexesForEdges(s, i);
-    }
-    auto [left, right] = getRepeatingSequenceIndexes_(s, i);
-    //std::cout << "Str size 1: " << s.size() << " " << left << " " << right << "\n";
-    if (left == 0 && right == s_size - 1)
-    {
-        //std::cout << "Whole string consist from one char\n";
-        return { left, right };
-    }
-    if (left > 0 && right < s_size && s[left - 1] != s[right + 1])
-    {
-        return { left, right };
-    }
-    while (left - 1 > -1 && right + 1 < s_size && s[left - 1] == s[right + 1])
+    while (left >= 0 && right < s.size() && s[left] == s[right])
     {
         left--;
         right++;
     }
-    return { left, right };
-}
-
-bool checkIfAllStringFromOneChar(const std::string& s)
-{
-    for (int i = 0; i < s.size() - 1; i++)
-    {
-        if (s[i] != s[i + 1])
-            return false;
-    }
-    return true;
+    return { left + 1, right - 1 };
 }
 
 std::string longestPalindrome(std::string s)
 {
     int s_size = s.size();
-    if (s_size == 1 || checkIfAllStringFromOneChar(s))
+    if (s_size <= 1)
         return s;
-    if (s_size == 2)
+
+    StrIndexes largest = { 0, 0 }, current;
+
+    for (int i = 0; i < s_size - 1; i++)
     {
-        if (s[0] == s[1])
-            return s;
-        else
-            return std::string( 1, s[0] );
-    }
-    StrIndexes largest = {0, 0}, current;
-    for (int i = 1; i < s.size() - 1; i++)
-    {
-        current = getIndexesForCurrent_(s, i);
-        if (current.start == 0 && current.end == s_size - 1)
-            return s;
-        if ((current.end - current.start) > (largest.end - largest.start))
+        current = getPalindromicIdxs(s, i, i);
+        if ((largest.end - largest.start) < (current.end - current.start))
             largest = current;
-        /*
-        std::cout << "Left is " << current.start
-                  << ", right is " << current.end
-                  << ", substring is " << getSubstring(s, current.start, current.end) << "\n";
-                  //*/
+
+        current = getPalindromicIdxs(s, i, i + 1);
+        if ((largest.end - largest.start) < (current.end - current.start))
+            largest = current;
     }
-    return getSubstring(s, largest.start, largest.end);
+
+    return s.substr(largest.start, largest.end - largest.start + 1);
 }
 
 namespace longest_pal
@@ -125,7 +55,15 @@ namespace longest_pal
             << longestPalindrome("dgshjagdgpolkyyyyyyyyyykl") << '\n' // should be lkyyyyyyyyyykl
             << longestPalindrome("cadabbuuuuuubbo") << '\n' // should be bbuuuuuubb
             << longestPalindrome("aaaa") << '\n'
-            << longestPalindrome("abbcccbbbcaaccbababcbcabca") << '\n'; // should be bbcccbbb
+            << longestPalindrome("abbcccbbbcaaccbababcbcabca") << '\n'; // should be bbcccbb 
     }
 
 }
+
+//TODO: wright independently after some time
+
+/*
+* Status: accepted
+* Runtime: 13ms, Beats 71.52%
+* Memory: 7.26 Mb, Beats 85.54%
+*/
